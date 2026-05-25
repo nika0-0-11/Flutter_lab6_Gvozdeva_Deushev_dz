fd47a62cbb40b6a3d2478b2a22a2c95d2df0254a
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-05-26 01:26:14 +0300

    Added widget for programming language

---
+++ lib/programming language.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Language extends StatelessWidget {
+  final String language;
+  const Language({super.key, required this.language});
+
+  @override 
+  Widget build(BuildContext context) {
+    return Text(
+      "Любимый язык программирования: $language",
+      style: const TextStyle(
+        fontSize: 32,
+        fontWeight: FontWeight.bold,
+        color: Colors.deepPurpleAccent,
+      ),
+    );
+  }
+}
